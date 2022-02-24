667453c78e9e4d9fc0f9b94d3221ce90e64014ef
S1lentSuzuka <x2785609571@outlook.com>
2022-02-24 17:28:28 +0800

    re
    er

--- assets/js/fetchG.js
+++ assets/js/fetchG.js
@@ -1,5 +1,5 @@
 fetchG = function(){
-	fetch('https://v1.hitokoto.cn/?c=b&c=c&c=a')
+	fetch('https://v1.hitokoto.cn/?c=b&c=c&c=a&c=d&c=e&c=f&c=a&c=g&c=h&c=i&c=j&c=k')
     .then(function (res){
       return res.json();
     })
