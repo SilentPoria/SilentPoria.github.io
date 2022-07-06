0b31bb0e0170cfd2c7c6ac2a13683c4714ee6226
S1lentSuzuka <x2785609571@outlook.com>
2022-07-06 22:07:55 +0800

    .

--- assets/js/appear.js
+++ assets/js/appear.js
@@ -1,8 +1,8 @@
-appear = function(){
+var i = 100
+appear = function(i){
     var test = document.getElementById("appear");
-          var count = (function() {
+          var count = (function(i) {
           var timer;
-          var i = 100;
               function change(tar) {
                   i--;
                   var num = 1-i/100;
@@ -17,5 +17,25 @@ appear = function(){
               }
               return change;
           })()
-         count(0)
+         count(500)
+}
+disappear = function(i){
+    var test = document.getElementById("appear");
+          var count = (function(i) {
+          var timer;
+              function change(tar) {
+                  i++;
+                  var num = 1-i/100;
+                  test.style.opacity=num;
+                  if (i === tar) {
+                      clearTimeout(timer);
+                      return false;
+                  }
+                  timer = setTimeout(function() {
+                      change(tar)
+                  }, 1)
+              }
+              return change;
+          })()
+         count(1000)
 }
